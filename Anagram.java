529ae8c71c35bfc599616aa760eb01dbb37ec369
asafbenhamo <asaf.benhamo@post.runi.ac.il>
2025-11-12 20:43:34 +0200

    ll

--- Anagram.java
+++ Anagram.java
@@ -1,3 +1,5 @@
+import javax.print.DocFlavor.STRING;
+
 /** Functions for checking if a given string is an anagram. */
 public class Anagram {
 	public static void main(String args[]) {
@@ -29,7 +31,22 @@ public class Anagram {
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
 		// Replace the following statement with your code
-		return false;
+		str1 = preProcess(str1);
+		str2 = preProcess(str2);
+		String s = "";
+	    String s1 = "";
+		int i = 0;
+		while (str2.length()>0) {
+			char c = str1.charAt(i);
+			if(str2.indexOf(c)== -1) return false;
+			else{
+				s = str2.substring(0, i);
+				s1 = str2.substring(i, str2.length());
+				str2 = s + s1;
+			}
+			i++;
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
@@ -37,13 +54,45 @@ public class Anagram {
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
 		// Replace the following statement with your code
-		return "";
+		str = str.toLowerCase();
+		String s = "";
+		String letter = "qwertyuioplkjhgfdsazxcvbnm";
+		for(int i = 0; i< str.length(); i++){
+			char c = str.charAt(i);
+			if ( letter.indexOf(c) != -1){
+            s = s + c;
+			}
+
+			
+		}
+		return s;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
 		// Replace the following statement with your code
-		return "";
+		str = preProcess(str);
+		String s = "";
+		String v = "";
+		char o;
+		for(int i = 0; i< str.length(); i++){
+			char c = str.charAt(i);
+			if ( str.indexOf(c) != -1){
+            s = s + c;
+			}
+		}
+			if (str == s){
+            o = s.charAt(0);
+            v = s.substring(1, s.length());
+		    s = v + o ;
+			}
+			if (isAnagram(str, s)== true){
+				return s;
+			}
+			return s;
+		}
+	
+	
+
 	}
-}
